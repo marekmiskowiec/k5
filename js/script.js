3a7657d7f76884fc9bfe1824183c5ad737034416
Marek Miśkowiec <miskowiec.ma@gmail.com>
2022-11-11 23:38:28 +0100

    Add const variables instead of functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function playGame(playerInput) {
+const playGame = function (playerInput) {
     clearMessages();
-    function getMoveName(argMoveId) {
+    const getMoveName = function (argMoveId) {
         if (argMoveId == 1) {
             return 'kamień';
         } else if (argMoveId == 2) {
@@ -13,7 +13,7 @@ function playGame(playerInput) {
         }
     }
 
-    function displayResult(argComputerMove, argPlayerMove) {
+    const displayResult = function (argComputerMove, argPlayerMove) {
         printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
         if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
             printMessage('Ty wygrywasz!');
@@ -37,11 +37,9 @@ function playGame(playerInput) {
             printMessage("Nie znam takiego ruchu :(");
         }
     }
-    //let playerMove = 'nieznany ruch';
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    let computerMove = getMoveName(randomNumber);
-    let playerMove = getMoveName(playerInput);
+    const randomNumber = Math.floor(Math.random() * 3 + 1),
+        computerMove = getMoveName(randomNumber),
+        playerMove = getMoveName(playerInput);
     displayResult(computerMove, playerMove);
 
 
